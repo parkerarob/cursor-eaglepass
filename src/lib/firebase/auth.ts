eb423c705b78c2812dd9fda86d98f60445eb45b1
parkerarob <robert.parker@nhcs.net>
2025-06-22 11:03:38 -0400

    Clean up auth.ts: remove unused useRouter import and signOutAndRedirect function. Fixes lint errors from previous commit.

--- src/lib/firebase/auth.ts
+++ src/lib/firebase/auth.ts
@@ -7,7 +7,6 @@ import {
   type User as FirebaseUser,
 } from 'firebase/auth';
 import { firebaseApp } from './config';
-import { useRouter } from 'next/navigation';
 
 export const auth = getAuth(firebaseApp);
 export const googleProvider = new GoogleAuthProvider();
@@ -19,14 +18,6 @@ googleProvider.setCustomParameters({
 export const signInWithGoogle = () => signInWithPopup(auth, googleProvider);
 
 export const signOut = () => firebaseSignOut(auth);
-
-export const signOutAndRedirect = () => {
-  const router = typeof window !== 'undefined' ? require('next/navigation').useRouter() : null;
-  return firebaseSignOut(auth).then(() => {
-    if (router) router.push('/');
-    else if (typeof window !== 'undefined') window.location.href = '/';
-  });
-};
 
 export { onAuthStateChanged };
 export type { FirebaseUser }; 
