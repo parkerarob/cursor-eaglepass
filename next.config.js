b0189735826b7b2347a2e15c0e89f449f42aaba4
parkerarob <robert.parker@nhcs.net>
2025-06-24 22:46:42 -0400

    fix(build): exclude redis packages from webpack client bundle

--- next.config.js
+++ next.config.js
@@ -4,17 +4,19 @@ const nextConfig = {
   webpack: (config, { isServer }) => {
     if (isServer) {
       config.externals = config.externals || [];
-      config.externals.push('firebase-functions');
+      config.externals.push('firebase-functions', 'redis', '@redis/client');
     }
     // Explicitly exclude functions directory
     config.resolve.alias = {
       ...config.resolve.alias,
       'firebase-functions': false,
+      redis: false,
+      '@redis/client': false,
     };
     return config;
   },
   // External packages for server components
-  serverExternalPackages: ['firebase-functions'],
+  serverExternalPackages: ['firebase-functions', 'redis', '@redis/client'],
 };
 
 module.exports = nextConfig; 
